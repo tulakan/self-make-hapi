e69ba0b86718cc1866c90ffbe92f05e6b58c41e5
Kanrawee Karaboot <tulakan17@gmail.com>
2017-03-11 18:33:55 +0700

    update hapi-mongodb

--- main.js
+++ main.js
@@ -1,5 +1,35 @@
 var Hapi = rquire('hapi');
 var server = new Hapi.Server(process.env.PORT || 3000);
+
+var dbOpts = {
+    url: 'mongodb://hapi:hapi@ds121980.mlab.com:21980/heroku_6kdqrqg7'
+}
+
+server.register({
+    register: require('hapi-mongodb'),
+    options: dbOpts
+}, function(err) {
+    if(err) {
+        console.error(err);
+        throw err;
+    }
+});
+
+server.route( {
+    method: 'GET',
+    path: '/temp/',
+    handler(request, reply) {
+        const db = request.mongo.db;
+
+        db.collection('temperature').find({} , function (err, result) {
+            if (err) {
+                return reply(Boom.internal('Internal MongoDB error', err));
+            }
+            reply(result);
+        });
+    }
+});
+
 server.start(function () {
     console.log('Server started at [' + server.info.uri + ']');
 });
