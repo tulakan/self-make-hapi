07ef0809aece3d467521a60654802314be0d7c6e
thanyaporn <kordnoon@gmail.com>
2017-03-12 23:14:38 +0700

    Update write file picture to db failed 3st
    path only

--- routes/uploadFiles.js
+++ routes/uploadFiles.js
@@ -9,31 +9,45 @@ exports.register = function(server, options, next) {
 
     const db = server.app.db;
 
+    // server.route({
+    //     method: 'POST',
+    //     path: '/uploadfiles',
+    //     config: {
+    //         payload: {
+    //             maxBytes: 209715200,
+    //             output: 'stream',
+    //             parse: false
+    //         },
+    //
+    //
+    //         handler: function(request, reply) {
+    //             var multiparty = require('multiparty');
+    //             var form = new multiparty.Form();
+    //             form.parse(request.payload, function(err, fields, files) {
+    //                 console.log(err);
+    //                 console.log(fields);
+    //                 console.log(files);
+    //             });
+    //
+    //         }
+    //     }
+    // });
+
     server.route({
         method: 'POST',
-        path: '/uploadfiles',
+        path: '/convert',
         config: {
             payload: {
+                output: 'file',
                 maxBytes: 209715200,
-                output: 'stream',
-                parse: false
+                //allow: 'multipart/form-data',
+                parse: true //or just remove this line since true is the default
             },
-    
-    
-            handler: function(request, reply) {
-                var multiparty = require('multiparty');
-                var form = new multiparty.Form();
-                form.parse(request.payload, function(err, fields, files) {
-                    console.log(err);
-                    console.log(fields);
-                    console.log(files);
-                });
-    
+            handler:function (request, reply) {
+                console.log('fileUpload path : ' + request.payload.fileUpload.path);
             }
-        }
+        },
     });
-
-    
     
     return next();
 };
